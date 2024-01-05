24fe4c60e52a81e489a1f06ee4d1532d1dca764e
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-01-05 14:06:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# benWoodardAssignment10
+ Coders Campus Assignment 10 - creating a meal plan with Java, 
---
+++ src/main/java/com/coderscampus/benWoodardAssignment10/BenWoodardAssignment10Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.benWoodardAssignment10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenWoodardAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenWoodardAssignment10Application.class, args);
+	}
+
+}
